84502bb1b99ff750c74bc1b38ffa5858bfc26d79
marcusmartins07 <marvyn0710@gmail.com>
2026-03-20 10:26:17 -0300

    Adicionado cors allowed

--- agrostore/settings.py
+++ agrostore/settings.py
@@ -27,9 +27,11 @@ INSTALLED_APPS = [
     'agrostore.pedidos',
     'agrostore.favoritos',
     'agrostore.avaliacoes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:5173'
+]
